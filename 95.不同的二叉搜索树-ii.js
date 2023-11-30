35a5c711ff595c89bb6a8c270d095daea6a1e2d5
yangjh1 <yangjh1@jiguang.cn>
2023-11-30 20:08:49 +0800

    二叉树算法

--- 95.不同的二叉搜索树-ii.js
+++ 95.不同的二叉搜索树-ii.js
@@ -17,8 +17,38 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
-
+var generateTrees = function (n) {
+  if (n == 0) return new Array();
+  // 构造闭区间 [1, n] 组成的 BST
+  return build(1, n);
 };
-// @lc code=end
 
+function build(left, right) {
+  // 存储 BST 的结果集
+  let res = new Array();
+  // base case
+  if (left > right) {
+    // 注意：这里必须添加 null，否则在下面无法递归左右子树
+
+    res.push(null);
+    return res;
+  }
+  // 1、穷举 root 节点的所有可能
+  for (var i = left; i <= right; i++) {
+    // 2、递归构造出左右子树的所有合法 BST
+    let leftTree = build(left, i - 1);
+    let rightTree = build(i + 1, right);
+    // 3、给 root 节点穷举所有左右子树的组合
+    for (var leftNode = 0; leftNode < leftTree.length; leftNode++) {
+      for (var rightNode = 0; rightNode < rightTree.length; rightNode++) {
+        // i 作为根节点 root 的值
+        var root = new TreeNode(i);
+        root.left = leftTree[leftNode];
+        root.right = rightTree[rightNode];
+        res.push(root);
+      }
+    }
+  }
+  return res;
+}
+// @lc code=end
--- 96.不同的二叉搜索树.js
+++ 96.不同的二叉搜索树.js
@@ -10,7 +10,20 @@
  * @return {number}
  */
 var numTrees = function(n) {
-    if(n===0 || n === 1)return n
+    let dp = new Array(n+1).fill(0).map(() => new Array(n+1).fill(0));
+    function count(left,right) {
+        if(left > right) return 1;
+        if(dp[left][right] !== 0) return dp[left][right];
+        let res = 0;
+        for(let i = left; i <= right; i++) {
+            let leftCount = count(left, i-1);
+            let rightCount = count(i+1, right);
+            res += leftCount * rightCount;
+        }
+        dp[left][right] = res;
+        return res
+    }
+    return count(1,n);
 };
 // @lc code=end
 
